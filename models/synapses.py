4073a59d745e9cc74d700936c27d42ef0191064b
amir <mr.amirentezari@gmail.com>
2024-03-26 14:03:15 +0330

    feat(model): define fully connected synapse model

--- models/synapses.py
+++ models/synapses.py
@@ -17,3 +17,25 @@ class SimpleSynapse(Behavior):
         sg.I = torch.sum(sg.W[pre_spike], axis=0) * self.alpha
 
 
+class FullyConnectedSynapse(Behavior):
+    """
+    Fully connected synapse class that connect all neurons in a source and destination.
+    """
+
+    def initialize(self, sg):
+        self.j0 = self.parameter("j0", None, required=True)
+        self.sigma = self.parameter("sigma", None, required=True)
+
+        self.N = sg.src.size + sg.dst.size
+
+        mean = self.j0 / self.N
+        variance = self.sigma / np.sqrt(self.N)
+        sg.W = sg.matrix(mode=f"normal({mean},{variance})")
+
+        sg.I = sg.dst.vector()
+
+    def forward(self, sg):
+        pre_spike = sg.src.spike
+        sg.I = torch.sum(sg.W[pre_spike], axis=0)
+
+
