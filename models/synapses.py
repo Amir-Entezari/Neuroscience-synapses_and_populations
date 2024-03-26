cad58a20284899931fe9f6a60178c644b2e8dfe8
amir <mr.amirentezari@gmail.com>
2024-03-26 14:04:18 +0330

    feat(model): define random connected with a fixed coupling probability synapse model

--- models/synapses.py
+++ models/synapses.py
@@ -39,3 +39,28 @@ class FullyConnectedSynapse(Behavior):
         sg.I = torch.sum(sg.W[pre_spike], axis=0)
 
 
+class RandomConnectedFixedProbSynapse(Behavior):
+    """
+    Random connected with fixed coupling probability synapse class that connect neurons in a source and destination
+    with a probability
+    """
+
+    def initialize(self, sg):
+        # Parameters:
+        self.j0 = self.parameter("j0", None, required=True)
+        self.variance = self.parameter("variance", None, required=True)
+        self.p = self.parameter("p", None, required=True)
+
+        self.N = sg.src.size + sg.dst.size
+
+        mean = self.j0 / (self.p * self.N)
+        # variance = self.p * (1 - self.p) * self.N
+        sg.W = sg.matrix(mode=f"normal({mean},{self.variance})")
+        sg.W[torch.rand_like(sg.W) > self.p] = 0
+        sg.I = sg.dst.vector()
+
+    def forward(self, sg):
+        pre_spike = sg.src.spike
+        sg.I = torch.sum(sg.W[pre_spike], axis=0)
+
+
