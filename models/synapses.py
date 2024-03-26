ed5e5d9e56069a10f4cce56f7ed7e5867ab1851f
amir <mr.amirentezari@gmail.com>
2024-03-26 14:04:42 +0330

    feat(model): define random fixed number of presynaptic partners connected a synapse model

--- models/synapses.py
+++ models/synapses.py
@@ -64,3 +64,41 @@ class RandomConnectedFixedProbSynapse(Behavior):
         sg.I = torch.sum(sg.W[pre_spike], axis=0)
 
 
+class RandomConnectedFixedInputSynapse(Behavior):
+    """
+    Random with fixed number of presynaptic partners connected synapse class that connect fixed number of neurons in
+    a source and destination.
+    """
+
+    def initialize(self, sg):
+        # Parameters:
+        self.j0 = self.parameter("j0", None, required=True)
+        self.variance = self.parameter("variance", None, required=True)
+        self.n = self.parameter("n", None, required=True)
+
+        self.N = sg.src.size + sg.dst.size
+
+        mean = self.j0 / self.n
+        sg.W = sg.matrix(mode=f"normal({mean},{self.variance})")
+
+        # Create a mask of which elements set to zero
+        mask = self.create_random_zero_mask(sg.W.shape, self.n)
+        # Apply the mask to the matrix
+        sg.W[mask] = 0
+
+        sg.I = sg.dst.vector()
+
+    def forward(self, sg):
+        pre_spike = sg.src.spike
+        sg.I = torch.sum(sg.W[pre_spike], axis=0)
+
+    def create_random_zero_mask(self, shape, n):
+        mask = torch.full(shape, True, dtype=torch.bool)
+        rows, cols = shape
+
+        for col in range(cols):
+            selected_rows = torch.randperm(rows)[:n]  # Randomly choose n indices for each column
+            mask[selected_rows, col] = False  # Set selected elements to 1
+
+        return mask  # Apply the mask to the original matrix
+
