193ad757214db4526e6000004ea8d2b05d5ca24c
amir <mr.amirentezari@gmail.com>
2024-03-27 02:24:52 +0330

    feat(model): define activity to record activity of populations

---
+++ models/activity.py
@@ -0,0 +1,9 @@
+from pymonntorch import Behavior
+
+class ActivityRecorder(Behavior):
+    def initialize(self, ng):
+        ng.activity = 0
+
+    def forward(self, ng):
+        num_spikes = ng.spike.sum()
+        ng.activity = num_spikes / ng.size
