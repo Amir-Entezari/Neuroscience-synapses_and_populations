59bf8286d4e23c95150e5ef67e335e45ecbeeb34
amir <mr.amirentezari@gmail.com>
2024-03-30 16:23:37 +0330

    refactor(model): set the initial value of activity as same as its forward.

--- models/activity.py
+++ models/activity.py
@@ -3,7 +3,8 @@ from pymonntorch import Behavior
 
 class ActivityRecorder(Behavior):
     def initialize(self, ng):
-        ng.activity = 0
+        num_spikes = ng.spike.sum()
+        ng.activity = num_spikes / ng.size
 
     def forward(self, ng):
         num_spikes = ng.spike.sum()
