5884d89b02da55f29274e618301a96afefe7c160
amir <mr.amirentezari@gmail.com>
2024-03-30 16:27:10 +0330

    refactor(model): sum up the currents in dendrite class. From now, if we have synapse, no need to add CurrentSum behavior.

--- models/dendrites.py
+++ models/dendrites.py
@@ -3,5 +3,6 @@ from pymonntorch import *
 
 class Dendrite(Behavior):
     def forward(self, ng):
+        ng.I += ng.inp_I
         for synapse in ng.afferent_synapses["All"]:
-            ng.I += synapse.I
+            ng.I += synapse.I # + ng.inp_I
